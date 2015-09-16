84732e80100532cee1ec5f1ed9c4d0ea658612a7
John Abreu <jboy1020@gmail.com>
2015-09-15 22:15:23 -0400

    FlyBy

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+	public Camera cardboardCamera;
+	private Vector3 currentPosition;
+	private float updatedPos;
+	public GameObject playerController;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if (cardboardCamera.transform.eulerAngles.y > 0) {
+//			currentPosition = playerController.transform.position;
+//			updatedPos = currentPosition.x + 0.2f;
+//			playerController.transform.position = new Vector3( updatedPos , currentPosition.y, currentPosition.z);
+//			Debug.Log ("updating pos " + updatedPos);
+//		}
+//		if (cardboardCamera.transform.eulerAngles.y < 0) {
+//			currentPosition = playerController.transform.position;
+//			updatedPos = currentPosition.x - 0.2f;
+//			playerController.transform.position = new Vector3( updatedPos , currentPosition.y, currentPosition.z);
+//			Debug.Log ("updating pos " + updatedPos);
+//		}
+//		if (cardboardCamera.transform.eulerAngles.x > 0) {
+//			currentPosition = playerController.transform.position;
+//			updatedPos = currentPosition.x + 0.2f;
+//			playerController.transform.position = new Vector3( updatedPos , currentPosition.y, currentPosition.z);
+//			Debug.Log ("pos " + updatedPos);
+//		}
+//		if (cardboardCamera.transform.eulerAngles.x < 0) {
+//			currentPosition = playerController.transform.position;
+//			updatedPos = currentPosition.x - 0.2f;
+//			playerController.transform.position = new Vector3( updatedPos , currentPosition.y, currentPosition.z);
+//			Debug.Log ("pos " + updatedPos);
+//		}
+	}
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+	public GameObject obstacle;
+	public GameObject powerup;
+	public GameObject boulder;
+	
+	float timeElapsed = 0;
+	float spawnCycle = .01f;
+	//bool spawnPowerup = true;
+	int spawnKind;
+	
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		spawnKind = Random.Range (0, 5);
+		if(timeElapsed > spawnCycle)
+		{
+			GameObject temp;
+
+			 if(spawnKind <=1)
+			{
+				temp = (GameObject)Instantiate(obstacle);
+				//Vector3 pos = temp.transform.position;
+				temp.transform.position = new Vector3(Random.Range(-15, 15),Random.Range(-10, 10) , 50);
+			}
+			
+			timeElapsed -= spawnCycle;
+			//spawnPowerup = !spawnPowerup;
+		}
+	}
+}
